dc215608df3ee64f9e8828e7fa22d41f9da35a65
Pablo Rodríguez Archilla <pablo.rodriguezarchilla@telefonica.com>
2016-03-08 16:23:08 +0100

    Add metric filtering by name when publishing to Monasca API

---
+++ ceilosca/ceilometer/publisher/monasca_metric_filter.py
@@ -0,0 +1,41 @@
+#
+# Copyright 2016 Telefónica Investigación y Desarrollo, S.A.U.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from oslo_log import log
+from ceilometer.i18n import _LI
+from ceilometer.publisher.monasca_data_filter import MonascaDataFilter
+
+LOG = log.getLogger(__name__)
+
+
+class MonascaMetricFilter(MonascaDataFilter):
+    """A specialization of MonascaDataFilter which allows filtering the metrics to be stored.
+
+    If the configuration file `monasca_field_definitions.yaml` includes a key `metrics`, then only the metrics whose
+    name is listed in that key will be stored; otherwise, metrics list is taken from `metadata` keys.
+    """
+
+    def __init__(self):
+        super(MonascaMetricFilter, self).__init__()
+        self._metrics = self._mapping.get('metrics', None) or self._mapping['metadata'].keys()
+
+    def process_sample_for_monasca(self, sample_obj):
+        """Returns a new metric built from the given sample, or `None` if discarded."""
+        metric = super(MonascaMetricFilter, self).process_sample_for_monasca(sample_obj)
+        if metric['name'] not in self._metrics:
+            LOG.debug(_LI("Discarded metric with name %s"), metric['name'])
+            metric = None
+
+        return metric
--- deployer/ceilosca.sh
+++ deployer/ceilosca.sh
@@ -16,77 +16,84 @@ export CEILOSCA_USER=$USER
 export TARGET_IP=127.0.0.1
 export MONASCA_API_URL=http://$TARGET_IP:8070/v2.0
 export CEILOSCA_DIR=${PWD}
-export CEILOSCA_FILES='ceilometer/monasca_client.py ceilometer/publisher/monasca_data_filter.py ceilometer/publisher/monclient.py ceilometer/storage/impl_monasca.py setup.cfg'
-export CEILOSCA_CONF_FILES='pipeline.yaml monasca_field_definitions.yaml';
+export CEILOSCA_CONF_FILES="pipeline.yaml monasca_field_definitions.yaml"
+export CEILOSCA_FILES="\
+    ceilometer/monasca_client.py \
+    ceilometer/publisher/monasca_data_filter.py \
+    ceilometer/publisher/monasca_metric_filter.py \
+    ceilometer/publisher/monclient.py \
+    ceilometer/storage/impl_monasca.py \
+    ceilometer/storage/impl_monasca_filtered.py \
+    setup.cfg \
+"
 
 clear_env()
 {
-        unset OS_USERNAME OS_PASSWORD OS_PROJECT_NAME OS_AUTH_URL
+    unset OS_USERNAME OS_PASSWORD OS_PROJECT_NAME OS_AUTH_URL
 }
 
 setup_devstack()
 {
-        git clone -b $PREFERRED_BRANCH $DEVSTACK_REPO $DEVSTACK_DIR || true
-        cp deployer/devstack/local.conf $DEVSTACK_DIR
-        cp deployer/devstack/ceilometer $DEVSTACK_DIR/lib
-        pushd $DEVSTACK_DIR
-        ./unstack.sh || true
-        ./stack.sh
-        popd
+    git clone -b $PREFERRED_BRANCH $DEVSTACK_REPO $DEVSTACK_DIR || true
+    cp deployer/devstack/local.conf $DEVSTACK_DIR
+    cp deployer/devstack/ceilometer $DEVSTACK_DIR/lib
+    pushd $DEVSTACK_DIR
+    ./unstack.sh || true
+    ./stack.sh
+    popd
 }
 
 install_ansible()
 {
-	      sudo apt-get install software-properties-common
-	      sudo apt-add-repository -y ppa:ansible/ansible
-	      sudo apt-get update
-	      sudo apt-get -y install ansible
+    sudo apt-get install software-properties-common
+    sudo apt-add-repository -y ppa:ansible/ansible
+    sudo apt-get update
+    sudo apt-get -y install ansible
 }
 
 get_monasca_files()
 {
-	      git clone $MONASCA_VAGRANT_REPO $WORK_DIR || true
-        pushd $WORK_DIR
-        ansible-galaxy install -r requirements.yml -p ./roles --ignore-errors
-        popd
+    git clone $MONASCA_VAGRANT_REPO $WORK_DIR || true
+    pushd $WORK_DIR
+    ansible-galaxy install -r requirements.yml -p ./roles --ignore-errors
+    popd
 }
 
 disable_monasca_ui_role()
 {
-        if [ -f $WORK_DIR/roles/monasca-ui/tasks/main.yml ]; then
-            rm $WORK_DIR/roles/monasca-ui/tasks/main.yml
-            file_list=$(find $WORK_DIR/roles -type f -exec grep -l get_url: {} +)
-            for filename in $file_list; do
-                sed -i.bak 's/ timeout=[0-9]\+//' $filename
-                sed -i '/get_url:/s/$/ timeout=600/' $filename
-            done
-        fi
-
+    if [ -f $WORK_DIR/roles/monasca-ui/tasks/main.yml ]; then
+        rm $WORK_DIR/roles/monasca-ui/tasks/main.yml
+        file_list=$(find $WORK_DIR/roles -type f -exec grep -l get_url: {} +)
+        for filename in $file_list; do
+            sed -i.bak 's/ timeout=[0-9]\+//' $filename
+            sed -i '/get_url:/s/$/ timeout=600/' $filename
+        done
+    fi
 }
 
 disable_monasca_events_installation()
 {
-        sed -i "s/- include: events.yml/#- include: events.yml/" $WORK_DIR/mini-mon.yml
+    sed -i "s/- include: events.yml/#- include: events.yml/" $WORK_DIR/mini-mon.yml
 }
 
 add_to_etc_hosts()
 {
-        if ! grep -q "$TARGET_IP devstack mini-mon" /etc/hosts; then
-            sudo bash -c "echo $TARGET_IP devstack mini-mon >> /etc/hosts"
-        fi
+    if ! grep -q "$TARGET_IP devstack mini-mon" /etc/hosts; then
+        sudo bash -c "echo $TARGET_IP devstack mini-mon >> /etc/hosts"
+    fi
 }
 
 add_monasca_ips_to_local_net_if()
 {
-        sudo ip addr add $DEVSTACK_IP/24 dev $NETWORK_IF || true
-        sudo ip addr add $MINIMON_IP/24 dev $NETWORK_IF || true
+    sudo ip addr add $DEVSTACK_IP/24 dev $NETWORK_IF || true
+    sudo ip addr add $MINIMON_IP/24 dev $NETWORK_IF || true
 }
 
 run_ceilosca()
 {
-        cd $WORK_DIR
-        ansible-playbook -u $CEILOSCA_USER -c local -k -i "devstack,"  devstack.yml
-        ansible-playbook -u $CEILOSCA_USER -c local -k -i "mini-mon,"  mini-mon.yml -e 'database_type=influxdb' -e 'influxdb_version=0.9.4.2'
+    cd $WORK_DIR
+    ansible-playbook -u $CEILOSCA_USER -c local -k -i "devstack,"  devstack.yml
+    ansible-playbook -u $CEILOSCA_USER -c local -k -i "mini-mon,"  mini-mon.yml -e 'database_type=influxdb' -e 'influxdb_version=0.9.4.2'
 }
 
 clear_env
