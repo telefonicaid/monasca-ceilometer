11fd8ef5e51e8cf2509fab19ed59b8f64d1f6f3b
pratid <pablo.rodriguezarchilla@telefonica.com>
2016-06-21 11:09:25 +0200

    Fix file encoding error

--- ceilosca/ceilometer/storage/impl_monasca_filtered.py
+++ ceilosca/ceilometer/storage/impl_monasca_filtered.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2016 Telefónica Investigación y Desarrollo, S.A.U.
+# Copyright 2016 Telefonica Investigacion y Desarrollo, S.A.U.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
